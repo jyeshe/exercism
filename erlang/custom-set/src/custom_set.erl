01afacc635279fb4d4e4ab2d120b4e0236fa9319
jyeshe <rppontual@gmail.com>
2019-02-24 19:12:57 -0300

    Erlang Custom Set type over map()

--- erlang/custom-set/src/custom_set.erl
+++ erlang/custom-set/src/custom_set.erl
@@ -1,54 +1,66 @@
 -module(custom_set).
 
--export([add/2, contains/2, difference/2, disjoint/2, empty/1, equal/2, from_list/1, intersection/2, subset/2,
-	 union/2]).
+-export([add/2, 
+		 contains/2, 
+		 difference/2, 
+		 disjoint/2, 
+		 empty/1, 
+		 equal/2, 
+		 from_list/1, 
+		 intersection/2, 
+		 subset/2,
+	 	 union/2]).
 
--type custom_set() :: list().
+-opaque set() :: #{type_key := custom_set}.
+-export_type([set/0]).
 
--spec add(Element :: any(), custom_set()) -> custom_set().
+%% API
+
+-spec add(Element :: any(), set()) -> set().
 add(Elem, Set) -> 
-	Contains = contains(Elem, Set),
-	if
-		Contains -> Set;
-		true -> [Elem | Set]
-	end.
+	maps:put(Elem, dummy, Set).
 
--spec contains(Element :: any(), custom_set()) -> boolean().
+-spec contains(Element :: any(), set()) -> boolean().
 contains(Elem, Set) -> 
-	lists:member(Elem, Set).
+	maps:find(Elem, Set) /= error.
 
--spec difference(Set1 :: custom_set(), Set2 :: custom_set()) -> custom_set().
+-spec difference(Set1 :: set(), Set2 :: set()) -> set().
 difference(Set1, Set2) -> 
-	Set1 -- Set2.
+	Map = maps:without(maps:keys(Set2), Set1),
+	assure_set_type(Map).
 
--spec disjoint(Set1 :: custom_set(), Set2 :: custom_set()) -> boolean().
+-spec disjoint(Set1 :: set(), Set2 :: set()) -> boolean().
 disjoint(Set1, Set2) -> 
 	difference(Set1, Set2) == Set1.
 
--spec empty(custom_set()) -> boolean().
+-spec empty(set()) -> boolean().
 empty(Set) -> 
-	Set == [].
+	maps:size(Set) == 1.
 
--spec equal(Set1 :: custom_set(), Set2 :: custom_set()) -> boolean().
+-spec equal(Set1 :: set(), Set2 :: set()) -> boolean().
 equal(Set1, Set2) -> 
-	length(Set1) == length(Set2) andalso lists:sort(Set1) == lists:sort(Set2).
+	maps:size(Set1) == maps:size(Set2) andalso empty(difference(Set1, Set2)).
 
--spec from_list(list()) -> custom_set().
+-spec from_list(list()) -> set().
 from_list(List) -> 
-	add_elements(List, []).
+	Set = assure_set_type(maps:new()),
+	lists:foldl(fun (Elem, Acc) -> add(Elem, Acc) end, Set, List).
 
--spec intersection(Set1 :: custom_set(), Set2 :: custom_set()) -> custom_set().
+-spec intersection(Set1 :: set(), Set2 :: set()) -> set().
 intersection(Set1, Set2) -> 
 	difference(Set1, union(difference(Set1, Set2), difference(Set2, Set1))).
 
--spec subset(Set1 :: custom_set(), Set2 :: custom_set()) -> custom_set().
+-spec subset(Set1 :: set(), Set2 :: set()) -> boolean().
 subset(Set1, Set2) -> 
-	difference(Set1, Set2) == [].
+	Set = maps:without(maps:keys(Set1), Set2),
+	maps:size(Set) == (maps:size(Set2) - maps:size(Set1)).
 
--spec union(Set1 :: custom_set(), Set2 :: custom_set()) -> custom_set().
+-spec union(Set1 :: set(), Set2 :: set()) -> set().
 union(Set1, Set2) -> 
-	add_elements(Set1, Set2).
+	maps:merge(Set1, Set2).
 
--spec add_elements(list(), Set :: custom_set()) -> custom_set().
-add_elements(List, Set) -> 
-	lists:foldl(fun (Elem, Acc) -> add(Elem, Acc) end, Set, List).
+%% Internal 
+
+-spec assure_set_type(map()) -> set().
+assure_set_type(Map) ->
+	maps:put(type_key, custom_set, Map).
