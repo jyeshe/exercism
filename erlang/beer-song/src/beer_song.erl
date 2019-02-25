572bdecb5a948889fb3c37020413537a3ccc6ee2
jyeshe <rppontual@gmail.com>
2019-02-24 23:01:54 -0300

    Erlang beer-song with List Comprehension.

---
+++ erlang/beer-song/src/beer_song.erl
@@ -0,0 +1,28 @@
+-module(beer_song).
+
+-export([verse/1, sing/1, sing/2]).
+
+verse(N) when N > 2 ->
+  NumStr = integer_to_list(N),
+  NextNumStr = integer_to_list(N-1),
+  NumStr ++ " bottles of beer on the wall, " ++ NumStr ++ " bottles of beer.\n" ++ 
+    "Take one down and pass it around, " ++ NextNumStr ++ " bottles of beer on the wall.\n";
+
+verse(2) ->
+  "2 bottles of beer on the wall, 2 bottles of beer.\n"
+  "Take one down and pass it around, 1 bottle of beer on the wall.\n";
+
+verse(1) ->
+  "1 bottle of beer on the wall, 1 bottle of beer.\n" ++
+  "Take it down and pass it around, no more bottles of beer on the wall.\n";
+
+verse(0) ->
+  "No more bottles of beer on the wall, no more bottles of beer.\n" ++
+  "Go to the store and buy some more, 99 bottles of beer on the wall.\n".
+
+sing(N) ->
+  sing(N, 0).
+
+sing(From, To) ->
+  Verses = [verse(I) || I <- lists:seq(From, To, -1)],
+  lists:join("\n", Verses) ++ "\n".
