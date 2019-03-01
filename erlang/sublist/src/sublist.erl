d9169f73951707c53a50e12fb66dfedabed022c6
jyeshe <rppontual@gmail.com>
2019-03-01 19:39:55 -0300

    Sublist with better call pattern matching.

--- erlang/sublist/src/sublist.erl
+++ erlang/sublist/src/sublist.erl
@@ -8,14 +8,11 @@ is_unequal(L1, L2) -> L1 /= L2.
 
 is_sublist([], _L2) -> true;
 is_sublist(_L1, []) -> false;
-is_sublist(L1 = [E1 | _Tail1], L2 = [_E2 | Tail2]) -> 
-    if
-        length(L1) > length(L2) ->
-            false;
-        true ->
-            BeginMatchL2 = lists:dropwhile(fun (E) -> E /= E1 end, L2),
-            lists:prefix(L1, BeginMatchL2) orelse is_sublist(L1, Tail2)
-    end.
+is_sublist(L1, L2) when length(L1) > length(L2) -> false;
+
+is_sublist(L1 = [E1 | _ ], L2 = [ _ | Tail2]) -> 
+    BeginMatchL2 = lists:dropwhile(fun (E) -> E /= E1 end, L2),
+    lists:prefix(L1, BeginMatchL2) orelse is_sublist(L1, Tail2).
 
 is_superlist([], []) -> true;
 is_superlist(L1, L2) -> is_sublist(L2,L1).
