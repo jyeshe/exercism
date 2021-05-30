2f602bd50aa31b8b849fee9332647fef8b08c7c1
Rogerio Pontual <rppontual@gmail.com>
2021-05-30 12:42:11 +0100

    erlang: nucleotide_count

---
+++ erlang/nucleotide-count/src/nucleotide_count.erl
@@ -0,0 +1,23 @@
+-module(nucleotide_count).
+
+-export([count/2, nucleotide_counts/1]).
+
+-define(IS_INVALID(N), N /= hd("A") andalso N /= hd("C") andalso N /= hd("G") andalso N /= hd("T")).
+
+count([], _Nucleotide) -> 0;
+count(Strand, [Nucleotide | _]) -> do_count(Strand, Nucleotide, 0).
+
+do_count([], _Nucleotide, Counter) -> Counter;
+do_count([N | _Strand], _Nucleotide, _Counter) when ?IS_INVALID(N) -> erlang:error(bad_nucleotide);
+do_count([N | Strand], Nucleotide, Counter) when N == Nucleotide -> do_count(Strand, Nucleotide, Counter+1);
+do_count([_N | Strand], Nucleotide, Counter) -> do_count(Strand, Nucleotide, Counter).
+
+nucleotide_counts(Strand) -> 
+    [
+        {"A", count(Strand, "A")}, 
+        {"C", count(Strand, "C")}, 
+        {"G", count(Strand, "G")}, 
+        {"T", count(Strand, "T")}
+    ].
+
+
